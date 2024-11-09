1393c91331486853fdf403a46f166be70ef9f01b
tenorioarandas <tenorio.arandas@gmail.com>
2024-11-09 14:15:30 -0300

    :recycle: refactor: Adição do prompt e melhora nas mensagens

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 'use strict';
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
+let maxNumber = parseInt(prompt("Choose the max number for the game:"));
+while (!maxNumber) {
+    parseInt(prompt("Invalid number! Choose another one:"))
+}
+
+let score = maxNumber;
+let secretNumber = Math.trunc(Math.random() * maxNumber) + 1;
 let highscore = 0;
+console.log(secretNumber);
+
+document.querySelector('.max-number').textContent = maxNumber;
+document.querySelector('.score').textContent = score;
 
 const displayMessage = function (message) {
     document.querySelector('.message').textContent = message;
@@ -13,34 +22,41 @@ document.querySelector('.check').addEventListener('click', function() {
     
     // When there is no input
     if (!guess) {
-        displayMessage('⛔ Invalid Number!');
+        displayMegitssage('⛔ Invalid Number!');
 
     // When player wins
     } else if (guess === secretNumber) {
 
-        displayMessage('🎉 Correct Number!');
+        displayMessage('🎉 You guessed it right!');
         document.querySelector('.number').textContent = secretNumber;
-        document.querySelector('h1').textContent = '🏆 You got it right!';
+        document.querySelector('h1').textContent = '🥇 Congratulations! You nailed it!';
         
 
-        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('body').style.backgroundColor = '#60b347';                           
         document.querySelector('.number').style.width = '30rem';
 
-        if (score > highscore) {
+        if (score === maxNumber) {
             highscore = score;
             document.querySelector('.highscore').textContent = highscore;
+            displayMessage('🏆 Max highscore unlocked!')
+            document.querySelector('h1').textContent = '🔥 Got it on the first try!';
+        } if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+            displayMessage('🚀 New highscore!')
+            document.querySelector('h1').textContent = '🌟 Highscore Achieved!';
         }
 
     // When guess is wrong
     } else if (guess !== secretNumber) {
         if (score > 1) {
-            displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+            displayMessage(guess > secretNumber ? '📈 Try a lower number.' : '📉 Try guessing higher.');
             score--;
             document.querySelector('.score').textContent = score;
         } else {
-            displayMessage('❌ You lost the game!');
+            displayMessage('💔 Better luck next time.');
             document.querySelector('.number').textContent = secretNumber;
-            document.querySelector('h1').textContent = '🔎 The secret number was';
+            document.querySelector('h1').textContent = '❌ The number you missed was';
             document.querySelector('.score').textContent = 0;
 
             document.querySelector('body').style.backgroundColor = '#FF3333';
@@ -50,8 +66,8 @@ document.querySelector('.check').addEventListener('click', function() {
 })
 
 document.querySelector('.again').addEventListener('click', function() {
-    secretNumber = Math.trunc(Math.random() * 20) + 1; 
-    score = 20;
+    secretNumber = Math.trunc(Math.random() * maxNumber) + 1; 
+    score = maxNumber;
 
     displayMessage('Start guessing...');
     document.querySelector('h1').textContent = 'Guess My Number!'
